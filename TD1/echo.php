186422b4910c8f8e2c5688f3e19e5955073beef8
Paulin-Zumstein <paulin.zumstein@etu.umontpellier.fr>
2020-09-11 14:30:25 +0200

    OPOPOrrr1

---
+++ TD1/echo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> Mon premier php </title>
+    </head>
+   
+    <body>
+        Voici le résultat du script PHP : 
+        <?php
+          // Ceci est un commentaire PHP sur une ligne
+          /* Ceci est le 2ème type de commentaire PHP
+          sur plusieurs lignes */
+           
+          // On met la chaine de caractères "hello" dans la variable 'texte'
+          // Les noms de variable commencent par $ en PHP
+          $texte = "hello world !";
+
+          // On écrit le contenu de la variable 'texte' dans la page Web
+          echo $texte;
+        ?>
+    </body>
+</html> 
