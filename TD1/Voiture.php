60935e574de0733bfb354d6f8b4478f153003b87
Paulin-Zumstein <paulin.zumstein@etu.umontpellier.fr>
2020-09-11 16:37:51 +0200

    voiture

--- TD1/Voiture.php
+++ TD1/Voiture.php
@@ -9,10 +9,29 @@ class Voiture {
   public function getMarque() {
        return $this->marque;  
   }
-     
+
+  public function getCouleur() {
+     return $this->couleur;  
+  }
+ 
+  public function getImmatriculation() {
+     return $this->immatriculation;  
+  }
+
   // un setter 
   public function setMarque($marque2) {
        $this->marque = $marque2;
+  }
+
+  public function setCouleur($couleur2) {
+       $this->couleur = $couleur2;
+  }
+
+  public function setImmatriculation($immatriculation2) {
+       if (strlen($immatriculation2)==8){
+          $this->immatriculation = $immatriculation2;     
+        }
+        else { echo "L'immatriculation n'a pas le bon nombre de chiffres"}
   }
       
   // un constructeur
@@ -24,7 +43,15 @@ class Voiture {
            
   // une methode d'affichage.
   public function afficher() {
-    // À compléter dans le prochain exercice
+    echo "<h1>Liste des voitures</h1>";
+          echo "<ol>";
+          if (empty($Voiture)) {
+            echo "pas voitures";
+          }
+          foreach ($Voiture as $key => $value) {
+            echo "<li>$key : $value </li>";
+          }
+          echo "</ol>"
   }
 }
 ?>
