2d0c70837595a3669977a03fb7a84b451c96691f
Paulin-Zumstein <paulin.zumstein@etu.umontpellier.fr>
2020-09-11 16:14:15 +0200

    commit num 2

--- TD1/echo.php
+++ TD1/echo.php
@@ -14,10 +14,32 @@
            
           // On met la chaine de caractères "hello" dans la variable 'texte'
           // Les noms de variable commencent par $ en PHP
-          $texte = "hello world !";
+          //$texte = "hello world !";
 
           // On écrit le contenu de la variable 'texte' dans la page Web
-          echo $texte;
+          //echo $texte;
+          /*$marque = "zoulou";
+          $couleur = "noir";
+          $immatriculation = "Tourquoin";
+          echo "<p> Voiture $immatriculation de marque $marque (couleur $couleur) </p>";*/
+
+          $Voiture = array ('marque' => 'zoulou','couleur' => 'noir', 'immatriculation' => 'Fr5322'  );
+          //$Voiture = array ();
+
+          /*foreach ($Voiture as $cle => $valeur){
+            echo "$cle : $valeur\n";
+          }*/
+
+          /*echo "<p> Voiture {$Voiture['immatriculation']} de marque {$Voiture['marque']} (couleur {$Voiture['couleur']}) </p>";*/ 
+          echo "<h1>Liste des voitures</h1>";
+          echo "<ol>";
+          if (empty($Voiture)) {
+            echo "pas voitures";
+          }
+          foreach ($Voiture as $key => $value) {
+            echo "<li>$key : $value </li>";
+          }
+          echo "</ol>"
         ?>
     </body>
 </html> 
