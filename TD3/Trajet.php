bc33cb23c85336672ba35e7958f3e77723d0c199
Paulin-Zumstein <paulin.zumstein@etu.umontpellier.fr>
2020-10-02 16:45:43 +0200

    tp3 presque fini

--- TD3/Trajet.php
+++ TD3/Trajet.php
@@ -60,4 +60,34 @@ class Trajet {
             die();
         }
     }
+    
+    public static function findPassagers($id) {
+        try {
+            $sql = "SELECT * FROM utilisateur
+                JOIN passager ON passager.utilisateur_login=utilisateur.login
+                JOIN trajet ON trajet.id=passager.trajet_id
+                JOIN utilisateur U ON U.login=trajet.conducteur_login
+                WHERE trajet.id=:id";
+            // Préparation de la requête
+            $req_prep = Model::$pdo->prepare($sql);
+            $values = array(
+                "id_tag" => $id
+            );
+            $req_prep->execute($values);
+        } catch (PDOException $e) {
+            if (Conf::getDebug()) {
+                echo $e->getMessage(); // affiche un message d'erreur
+            } else {
+                echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+            }
+            die();
+            $req_prep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
+            $tab_users = $req_prep->fetchAll();
+            return $tab_users;
+            // Attention, si il n'y a pas de résultats, on renvoie false
+            /* if (empty($tab_voit))
+              return false;
+              return $tab_voit[0]; */
+        }
+    }
 }
---
+++ TD3/testFindUtil.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'Utilisateur.php';
+require_once 'Voiture.php';
+require_once 'Trajet.php';
+
+Trajet::
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
