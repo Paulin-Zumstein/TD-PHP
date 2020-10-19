35e4147a34944210a65319ce2e73b8fd1d504148
Paulin-Zumstein <paulin.zumstein@etu.umontpellier.fr>
2020-10-19 12:50:54 +0200

    ex6 fini

--- TD5/view/voiture/list.php
+++ TD5/view/voiture/list.php
@@ -7,7 +7,7 @@
     <body>
         <?php
         foreach ($tab_v as $v)
-            echo '<p> Voiture d\'immatriculation <a href="https://webinfo.iutmontp.univ-montp2.fr/~zumsteinp/PHP/TD-PHP/TD5/index.php?action=read&immatriculation=' . $v->getImmatriculation() . '">'. htmlspecialchars($v->getImmatriculation()) .'</a> .</p>';
+            echo '<p> Voiture d\'immatriculation <a href="https://webinfo.iutmontp.univ-montp2.fr/~zumsteinp/PHP/TD-PHP/TD5/index.php?action=read&immatriculation=' . rawurlencode($v->getImmatriculation()) . '">'. htmlspecialchars($v->getImmatriculation()) .'</a> .</p>';
         ?>
     </body>
 </html>
